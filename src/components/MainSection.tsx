e9b09ec7b2e89b4d26a9d684b44a481390e07ee1
rose12346code <stephanie48rose@gmail.com>
2026-01-09 23:30:39 +0100

    same comment

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -47,7 +47,7 @@ export default function MainSection() {
 
     
   return (
-    <div className='w-[92%] md:grid  space-y-16  md:grid-cols-2 lg:grid-cols-3 items-center  lg:pl-4 lg:py-4 md:ml-[50px]  ml-[25px] lg:ml-[350px] lg:gap-1 '>
+    <div className='w-[92%] md:grid  space-y-16  md:grid-cols-2 lg:grid-cols-3 items-center  lg:pl-4 lg:py-4 md:ml-[50px]  ml-[25px] lg:ml-[310px] lg:gap-1 '>
 
         {isLoading? (<ProductsSkeleton />):
             (productCollection.map( product=>(
