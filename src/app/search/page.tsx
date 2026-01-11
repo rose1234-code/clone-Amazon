dd00ef90aa4d0bad3aa7163956d725512bffeb9a
rose12346code <stephanie48rose@gmail.com>
2026-01-11 13:10:11 +0100

    finally research

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -4,11 +4,9 @@ import { productState } from '@/components/MainSection'
 import NavBar from '@/components/NavBar'
 import Search from '@/components/Search'
 import axios from 'axios'
-import Link from 'next/link'
 import { useSearchParams } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
-export const dynamic = 'force-dynamic'
 
 export default function page() {
     const variable=useSearchParams().get('query')
@@ -33,11 +31,11 @@ export default function page() {
    
         <div className="w-[95%] mx-auto px-4 mt-12">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-                {listProduct.map(product => (
+                { listProduct.length >0 ? listProduct.map(product => (
                     <div key={product.id} className="group bg-white rounded-2xl shadow-md hover:shadow-xl transition-all duration-300 overflow-hidden">
                         <Search image={product.thumbnail} price={product.price} title={product.title}/>
                     </div>
-                ))}
+                )): (<div>no product for your search</div>)}
             </div>
         </div>
 
