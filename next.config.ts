dc66ad47eb5bb44bf03e9863db8b3f639d59e13c
rose12346code <stephanie48rose@gmail.com>
2026-01-11 15:11:12 +0100

    update command button

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-
-
-  images: {
-    domains: ['cdn.dummyjson.com'],
-}
-
 };
 // module.exports = nextConfig;
 export default nextConfig;
